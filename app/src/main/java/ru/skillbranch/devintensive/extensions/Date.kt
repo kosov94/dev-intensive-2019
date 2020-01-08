87bff2a62dd49e5ee4f91014792d1321d5224ca7
Victor <kosov941@gmail.com>
2020-01-08 12:01:06 +0700

    Date formate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,7 +1,13 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun Date.humanizeDiff(date:Date= Date()):String{
     return "tyt data"
+}
+
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String{
+    val dataFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dataFormat.format(this)
 }
