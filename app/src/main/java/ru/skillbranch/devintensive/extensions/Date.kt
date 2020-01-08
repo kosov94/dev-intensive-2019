10e2ba7e4fbb5b994e87fde3f7a151f53e726466
Victor <kosov941@gmail.com>
2020-01-08 12:27:58 +0700

    Date add

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -3,11 +3,37 @@ package ru.skillbranch.devintensive.extensions
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Date.humanizeDiff(date:Date= Date()):String{
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+fun Date.humanizeDiff(date: Date = Date()): String {
     return "tyt data"
 }
 
-fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String{
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dataFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dataFormat.format(this)
+}
+
+fun Date.add(value: Int, units: TimeUnits): Date {
+    var time = this.time
+    time += when (units) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+
+    this.time = time
+    return this
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
 }
