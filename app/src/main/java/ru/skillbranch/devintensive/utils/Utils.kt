57646e8ac58ef615fbf5dd6ec9a077a15e285933
Victor <kosov941@gmail.com>
2020-01-08 13:19:34 +0700

    Utils toInitials

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -7,4 +7,22 @@ object Utils {
         val lastName = if (pair?.getOrNull(1)?.length != 0) pair?.getOrNull(1) else null
         return Pair(firstName, lastName)
     }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        var initials: String? = ""
+        if (firstName != null && lastName != null) {
+            if (firstName.length != 0 && firstName[0] > ' ') initials += firstName[0].toUpperCase()
+            if (lastName.length != 0 && lastName[0]>' ') initials += lastName[0].toUpperCase()
+        } else
+            if (firstName != null)
+                initials += firstName[0].toUpperCase()
+            else
+                if (lastName != null)
+                    initials += lastName[0].toUpperCase()
+                else
+                    initials = null
+        if(initials.equals(""))
+            return null
+        return initials
+    }
 }
