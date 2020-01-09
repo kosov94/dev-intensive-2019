ff518fad03fbb48dbe9c8d8ee20786dc124c96b7
Victor <kosov941@gmail.com>
2020-01-09 12:47:37 +0700

    String stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -7,12 +7,39 @@ fun String.truncate(length: Int = 16): String {
     if (string.length <= length) {
         for (element in string)
             str += element
-    }
-    else{
-        for(i in 0 until length)
-            str+=string[i]
-        str=str.trim()
-        str+="..."
+    } else {
+        for (i in 0 until length)
+            str += string[i]
+        str = str.trim()
+        str += "..."
     }
     return str
+}
+
+fun String.stripHtml(): String {
+    var str = ""
+    val string: String = this.trim()
+    var i: Int = 0
+    while (i < string.length) {
+        when {
+            string[i] == '<' -> {
+                while (string[i] != '>')
+                    i++
+                i++
+            }
+            string[i] == ' ' -> while (string[i + 1] == ' ')
+                i++
+            string[i] == '&' -> {
+                while (string[i] != ';')
+                    i++
+                i++
+            }
+        }
+
+        if (i < string.length)
+            str += string[i]
+        i++
+    }
+    return str
+
 }
